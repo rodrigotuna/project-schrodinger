f9c333a3f016e365c05865f2d88bb1b892c29ad6
Francisco Ana <franciscopdana@gmail.com>
2022-04-06 14:06:19 +0100

    Changed some visual details

--- app_feup/lib/view/Widgets/copy_center_card.dart
+++ app_feup/lib/view/Widgets/copy_center_card.dart
@@ -14,13 +14,14 @@ class CopyCenterCard extends GenericCard {
     return Column(
       children: <Container>[
         h1('Horário', context, initial: true),
-        h2('Piso -1 do edifício B | Edifício da AEFEUP.', context),
+        h2('Piso -1 do edifício B | Edifício da AEFEUP', context),
         infoText('9:00h - 11:30h | 12:30h - 18:00h', context),
         h1('Telefone', context),
-        h2('FEUP: ', context),
+        h2('FEUP ', context),
         infoText('+351 220 994 122', context, link: 'tel:220 994 122'),
-        h2('AEFEUP: ', context),
-        infoText('+351 220 994 132', context, link: 'tel:220 994 132'),
+        h2('AEFEUP ', context),
+        infoText('+351 220 994 132', context,
+            link: 'tel:220 994 132', last: true),
       ],
     );
   }
--- app_feup/lib/view/Widgets/multimedia_center_card.dart
+++ app_feup/lib/view/Widgets/multimedia_center_card.dart
@@ -14,8 +14,8 @@ class MulimediaCenterCard extends GenericCard {
     return Column(
       children: <Container>[
         h1('Horário', context, initial: true),
-        h2('B123', context),
-        infoText('9:00h – 12:30h | 14:30h – 17:00h', context),
+        h2('Sala B123', context),
+        infoText('9:00h - 12:30h | 14:30h - 17:00h', context),
         h1('Telefone', context),
         infoText('+351 225 081 466', context, link: 'tel:225 081 466'),
         h1('Email', context),
